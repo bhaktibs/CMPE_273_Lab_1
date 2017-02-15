11fe78c49f5c3647c1c0cf54fa06ebef44708ed3
System Administrator <root@bhaktis-MacBook-Air.local>
2017-02-15 12:09:58 -0800

    first Commit

--- socket-mon.py
+++ socket-mon.py
@@ -5,31 +5,29 @@ from collections import Counter
 
 
 socket_connections = psutil.net_connections()
-#print "I am printing socket_connections\n",socket_connections
-#print type(socket_connections)
+
 socket_connection_tu = (socket_connections[1])
-#print (socket_connection_tu[3])
+
 final_dic={}
 
 for a in socket_connections:
     if a.laddr != ('0.0.0.0', 0) and a.raddr != ():
-    #print a.pid
-        final_list=[a.laddr,a.raddr,a.status]
+        laddr_split=list(a.laddr)
+        raddr_split=list(a.raddr)
+        laddr_final = laddr_split[0]+"@"+str(laddr_split[1])
+        raddr_final=raddr_split[0]+"@"+str(raddr_split[1])
+        final_list=[laddr_final,raddr_final,a.status]
         if not a.pid in final_dic:
-            final_dic[a.pid] = [final_list]
+                final_dic[a.pid] = [final_list]
         else:
-            final_dic[a.pid].append(final_list)
-new_dic = {}
+                final_dic[a.pid].append(final_list)
+    new_dic = {}
 for keys in final_dic:
     new_dic[keys] = len(final_dic[keys])
     
-
+print '"Pid","laddr","raddr","Status"'
 for key, value in sorted(new_dic.iteritems(), key=lambda (k,v): (v,k), reverse=True):
-    for a in final_dic[key]:
-        print "%s: %s" % (key, a)
-        #print "\n"
-print new_dic
-
-
-#[list(group) for key,group in itertools.groupby(socket_connections,operator.itemgetter(1))]
+    for a in final_dic[key]: 
+        print '"%s", "%s", "%s", "%s"' % (key,a[0],a[1],a[2]) 
+   
 
